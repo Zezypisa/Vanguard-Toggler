185b9a2436ebeee386462d4b5a72168fd13f4052
Zezypisa <49347779+Zezypisa@users.noreply.github.com>
2021-03-05 18:53:21 -0500

    v1

---
+++ Vanguard Toggler.bat
@@ -0,0 +1,37 @@
+@echo OFF
+title Vanguard Toggler
+cls
+:home
+cls
+echo Vanguard Toggler created by Zezypisa
+set /p select=Would you want to stop or start vanguard? 
+if "%select%" == "start" goto startVG
+if "%select%" == "stop" goto stopVG
+if "%select%" == "exit" exit
+echo Not a valid option. start \ stop \ exit
+pause
+goto home
+
+:startVG
+	sc config vgk start=system
+	sc config vgc start=demand
+	goto restartprompt
+:stopVG
+	sc config vgk start=disabled 
+	sc config vgc start=disabled
+	goto restartprompt
+
+:restartprompt
+set /p select=Would you like to restart now? 
+if %select%==yes (
+	echo Ok, shutting down
+	shutdown /p /r
+)
+if %select%==no (
+	echo Ok, Exiting now
+	pause
+	exit
+)
+echo Not a valid option. yes \ no
+pause
+goto restartprompt
